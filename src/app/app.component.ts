371d83de4d40c1e60e7bbe7d36224556a41b9f2f
João Victor <joaovictorslf@gmail.com>
2022-05-12 23:22:58 -0300

    add correct title to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import * as Leaflet from 'leaflet';
 
 @Component({
   selector: 'app-root',
@@ -7,21 +6,5 @@ import * as Leaflet from 'leaflet';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  title = 'open-layers-test';
-
-  ngOnInit(): void {
-    // const map = Leaflet.map('map', {
-    //   center: [43, 19],
-    //   zoom: 4,
-    //   scrollWheelZoom: false,
-    //   zoomControl: false,
-    //   touchZoom: false,
-    //   dragging: false,
-    // });
-    // Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    //   attribution:
-    //     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    // }).addTo(map);
-    // Leaflet.marker([43, 19]).addTo(map);
-  }
+  ngOnInit(): void {}
 }
